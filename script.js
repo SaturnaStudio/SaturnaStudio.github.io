9dfa72786888cffb486757e4078d20a95b16a567
Saturna19-fr <saturna19.stream@outlook.com>
2023-12-21 21:01:05 +0100

    WBH + Prenom

--- script.js
+++ script.js
@@ -3,16 +3,28 @@ const prenom = document.getElementById('prenom')
 const nom = document.getElementById('nom')
 elem.addEventListener('click', sendDiscord)
 
+var myEmbed = {
+    author: {
+      name: "Captain Hook"
+    },
+    title: "My new embed",
+    description: "This is a cool-looking Discord embed, sent directly from JavaScript!",
+    color: hexToDecimal("#ff0000")
+  }
+
 function sendDiscord(){
     const request = new XMLHttpRequest();
     request.open("POST", "https://discord.com/api/webhooks/1187438610149015553/RipIqS6ho9psGQZswuPXcehHGrgsw26OAaxN1U0_YQOfGxa96wqi5KfgHUFf-jqRkE-Q");
     request.setRequestHeader('Content-type', 'application/json');
     let params = {
-        username: "My Webhook Name",
-        avatar_url: "",
-        content: prenom.value
-        
+        content: "@everyone",
+        embeds: [
+            {
+                title: "Nouvelle recherche",
+                description: "Nom: "+ (nom.value || "Vide") + "\nPrénom: " + (prenom.value || "Vide")
+            }
+        ]
     }
     request.send(JSON.stringify(params));
-
+    alert("Aucune correspondance n'a été trouvée.")
 }
